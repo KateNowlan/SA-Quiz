2ee4e76da34491d49b312fb8eddc0530a71d96df
KateNowlan <kate.turner26@gmail.com>
2022-04-26 07:32:09 +0000

    add code to save the score on end.js

---
+++ assets/js/end.js
@@ -0,0 +1,8 @@
+/* jshint esversion: 6 */
+//Create basic const functions
+const username = document.getElementById("username");
+
+//save the score
+saveHighScore = e => {
+    e.preventDefault()
+};
