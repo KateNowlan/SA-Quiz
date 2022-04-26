e583f879ca4353ed1efdeeeb3f010d5360f01ea9
KateNowlan <kate.turner26@gmail.com>
2022-04-26 07:33:36 +0000

    add event listener for key up on savescore button

--- assets/js/end.js
+++ assets/js/end.js
@@ -1,6 +1,13 @@
 /* jshint esversion: 6 */
 //Create basic const functions
 const username = document.getElementById("username");
+const saveScoreBtn = document.getElementById("saveScoreBtn");
+
+
+//add event listener for username
+username.addEventListener('keyup', () => {
+    saveScoreBtn.disabled = !username.value;
+});
 
 //save the score
 saveHighScore = e => {
